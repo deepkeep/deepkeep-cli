6840fbd55419e749dbac1929d30d161b8dcdba0a
Fredrik Noren <noren@spotify.com>
2015-08-03 20:24:50 -0400

    Basic publish

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var FormData = require('form-data');
 var read = require("read");
 var promzard = require('promzard');
 var path = require('path');
-var promptFile = path.resolve(__dirname, 'prompt.js');
 
 var parser = require("nomnom");
 
@@ -12,23 +11,15 @@ parser.command('init')
   .callback(init)
   .help('initializes a new package.json');
 
-parser.command('zip')
-  .option('outfile', {
-    abbr: 'o',
-    default: 'package.zip',
-    help: 'name of the zip file to write to'
-  })
-  .callback(zip.bind(null))
-  .help('zip the current project for publishing');
-
 parser.command('publish')
   .callback(publish)
   .help('publishes a package to deepkeep');
 
 parser.parse();
 
-function init() {
-  promzard(promptFile, {}, function (err, data) {
+function init(opts) {
+  var initDialogFile = path.resolve(__dirname, 'init-dialog.js');
+  promzard(initDialogFile, {}, function (err, data) {
     if (err) throw new Error(err);
 
     var packageJson = path.join(__dirname, 'package.json');
@@ -50,67 +41,45 @@ function init() {
   });
 }
 
-function zip(options, cb) {
-  var outfile = options.outfile || 'package.zip';
-  var packageJson = path.join(__dirname, 'package.json');
-  fs.readFile(packageJson, function(err, data) {
-    if (err) {
-      console.log('Failed to read ' + packageJson, err);
-      process.exit(1);
-    }
+function publish(opts) {
+  var packageJson = JSON.parse(fs.readFileSync('package.json'));
+  if (!packageJson.name) throw new Error('name required in package.json');
+  if (!packageJson.version) throw new Error('version required in package.json');
 
-    var packageModel = JSON.parse(data);
-    if (!packageModel.model) {
-      console.log('"model" is undefined in ' + packageJson + ' but required');
-      process.exit(1);
-    }
+  var zip = new AdmZip();
+  zip.addLocalFile('package.json', 'package.json');
+  if (fs.existsSync('README.md'))
+    zip.addLocalFile('README.md', 'README.md');
+  if (fs.existsSync('network.t7'))
+    zip.addLocalFile('network.t7', 'network.t7');
+  var packageZip = zip.toBuffer();
 
-    var modelPath = path.join(__dirname, packageModel.model);
-    fs.exists(modelPath, function(exists) {
-      if (!exists) {
-        console.log(modelPath + ' does not exist, but is required');
-        process.exit(1);
-      }
-      var zip = new AdmZip();
-      // TODO falcon: add readme or should we just package the whole directory?
-      zip.addLocalFile(packageJson);
-      zip.addLocalFile(modelPath);
-      if (cb) {
-        zip.toBuffer(cb);
-      } else {
-        return zip.writeZip(outfile);
-      }
-    });
-  });
-}
-
-function publish() {
-  zip({}, function(zipFile) {
-    read({prompt:'Username: '}, function(err, username) {
+  read({prompt:'Username: '}, function(err, username) {
+    if (err) process.exit(1);
+    read({prompt: 'Password: ', silent: true}, function(err, password) {
       if (err) process.exit(1);
-      read({prompt: 'Password: ', silent: true}, function(err, password) {
-        if (err) process.exit(1);
 
-        var form = new FormData();
-        form.append('package', zipFile, {
-          filename: 'package.zip',
-          contentType: 'application/octet-stream',
-          knownLength: zipFile.length
-        });
+      var form = new FormData();
+      form.append('package', packageZip, {
+        filename: 'package.zip',
+        contentType: 'application/octet-stream',
+        knownLength: packageZip.length
+      });
+      var formOpts = {
+        host: opts.host || 'packages.deepkeep.co',
+        port: opts.port || 80,
+        path: '/v1/upload',
+        auth: username + ':' + password
+      }
 
-        form.submit({
-          host: 'localhost',
-          port: 8080,
-          path: '/api/v1/upload',
-          auth: username + ':' + password
-        }, function(err, res) {
-          if (err) {
-            console.log('Failed to upload package');
-          } else {
-            console.log('Successfully uploaded package to deepkeep!');
-          }
-          res.resume();
-        });
+      console.log('Uploading', username + '/' + packageJson.name + '/' + packageJson.version, 'to', formOpts.host + (formOpts.port != 80 ? ':' + formOpts.port : ''));
+      form.submit(formOpts, function(err, res) {
+        if (err || res.statusCode !== 200) {
+          console.log('Failed to upload package', err, res.statusMessage, res.body);
+        } else {
+          console.log('Successfully uploaded package to deepkeep!');
+        }
+        res.resume();
       });
     });
   });
--- init-dialog.js
+++ init-dialog.js
@@ -2,7 +2,5 @@ var fs = require('fs');
 var path = require('path');
 module.exports = {
   name: prompt('name', path.basename(__dirname)),
-  version: prompt('version', '0.0.0'),
-  description: prompt(),
-  model: prompt()
+  version: prompt('version', '0.0.0')
 }
